ec23eb15c5bf216eadb16ccbf055882a0911eebe
alexander <petrovodessa1936@gmail.com>
2022-06-28 23:51:10 +0300

    Contstruct and size

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> customListWithValues = new CustomArrayList<>(Arrays.asList("Test1", "Test2"));
+        List<String> customListWithoutValuesAndCapacuty = new CustomArrayList<>();
+        List<String> customListWithCapacity = new CustomArrayList<>(5);
+
+        System.out.println(customListWithValues.size());
+        System.out.println(customListWithCapacity.size());
     }
 }
